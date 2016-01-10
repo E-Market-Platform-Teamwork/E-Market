e106b73821416c409cdc64c5a8df5078c873507a
VladimirDimov <vdimov1124@gmail.com>
2016-01-10 14:59:45 +0200

    about files added

---
+++ Skeleton/server/config/routers/about-router.js
@@ -0,0 +1,10 @@
+var router = require('express').Router(),
+    controllers = require('../../controllers');
+
+router
+    .get('/', controllers.about.get)
+    .post('', controllers.about.add)
+
+module.exports = function (app) {
+    app.use('/about', router);
+}
