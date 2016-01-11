dd29f3041a15aee228b02a5152bf888fac349c8b
Margarita Ivancheva <mvivancheva@gmail.com>
2016-01-11 14:46:53 +0200

    resolved minor conflicts

--- Skeleton/server/config/routers/about-router.js
+++ Skeleton/server/config/routers/about-router.js
@@ -2,15 +2,10 @@ var router = require('express').Router(),
     controllers = require('../../controllers');
 
 router
-<<<<<<< HEAD
-    //.get('/', controllers.about.get)
-    //.post('', controllers.about.add)
-=======
     .get('/', controllers.about.getAbout)
     .get('/edit', controllers.about.getEdit)
     .post('/', controllers.about.edit)
     .post('/uploadImage', controllers.about.uploadImage)
->>>>>>> refs/remotes/origin/master
 
 module.exports = function (app) {
     app.use('/about', router);
