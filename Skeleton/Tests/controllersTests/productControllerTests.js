90efea837b6a207a3eb2fb20d41941810df3c20e
GoranTelerik <gorancvetkov_sr@yahoo.com>
2016-01-14 14:23:58 +0200

    Some products test added!

--- Skeleton/Tests/controllersTests/productControllerTests.js
+++ Skeleton/Tests/controllersTests/productControllerTests.js
@@ -1,3 +1,35 @@
-/**
- * Created by Goran on 1/14/2016.
- */
+var url = 'http://localhost:3000',
+    chai = require('chai'),
+    chaiHttp = require('chai-http'),
+    expect = chai.expect,
+    sinon = require('sinon'),
+    sinonChai = require('sinon-chai'),
+    fakes = require('../fakesServices/fakeServices'),
+    productsController = require('../../server/controllers/productsController')( fakes.products);
+
+chai.use(chaiHttp);
+chai.use(sinonChai);
+
+describe('productsController', function () {
+    describe('get', function () {
+            it('should render first page of products!', function () {
+            var res = {
+                redirect: function () {
+                },
+                render: function () {
+                }
+            };
+
+            var req = {};
+            var next = function () {
+            };
+
+            sinon.spy(res, 'render');
+            sinon.spy(res, 'redirect');
+
+            productsController.getAll(req, res, next);
+
+            expect(res.render.calledWith('products/products')).to.be.true;
+        });
+    })
+});
--- Skeleton/Tests/fakesServices/fakeServices.js
+++ Skeleton/Tests/fakesServices/fakeServices.js
@@ -1,3 +1,25 @@
-/**
- * Created by Goran on 1/14/2016.
- */
+module.exports = {
+    products: {
+        getAll: function(){
+            return {};
+        },
+        createProduct: function (product, callback) {
+            var createdProduct = {};
+            callback(null, createdProduct);
+        },
+        getLatestProducts: function (callback) {
+            var latestProducts = {
+                docs: [
+                    {
+                        id: 1
+                    },
+                    {
+                        id: 2
+                    }
+                ]
+            };
+
+            callback(null, latestProducts);
+        }
+    }
+};
